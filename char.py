cfdb362400a87bb17d0bd534987687454c07dae1
Lucas <lcdsdlima@inf.ufpel.edu.br>
2015-06-13 12:09:22 -0300

    criando arquivo 10 mega

---
+++ char.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+#
+from random import randint
+for j in range(0,500):
+	for i in range(0,10000):
+		valor=randint(33,127)
+		caractere=str(unichr(valor))
+		print caractere
+	print '\n'
+
+		
+	
